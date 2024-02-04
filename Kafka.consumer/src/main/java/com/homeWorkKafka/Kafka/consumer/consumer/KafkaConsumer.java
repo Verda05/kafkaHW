56fee4ae133b02772c2161a1035969a9e6c04d04
Batyrkhan <92648843+Verda05@users.noreply.github.com>
2024-02-04 20:56:39 +0300

    HW Kafka v1.2

--- Kafka.consumer/src/main/java/com/homeWorkKafka/Kafka/consumer/consumer/KafkaConsumer.java
+++ Kafka.consumer/src/main/java/com/homeWorkKafka/Kafka/consumer/consumer/KafkaConsumer.java
@@ -2,6 +2,11 @@ package com.homeWorkKafka.Kafka.consumer.consumer;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.annotation.PartitionOffset;
+import org.springframework.kafka.annotation.TopicPartition;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.handler.annotation.Header;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Service;
 
 @Slf4j
@@ -10,8 +15,18 @@ public class KafkaConsumer {
 
     public static final String TOPIC_NAME = "kafka.example.topic";
 
-    @KafkaListener(topics = TOPIC_NAME)
-    public void consume(String message){
+    @KafkaListener(topics = TOPIC_NAME,
+    topicPartitions = @TopicPartition(topic = TOPIC_NAME,
+    partitionOffsets = @PartitionOffset(partition = "0", initialOffset = "0")))
+    public void consume(String message) {
         log.info("Receive new message! {}", message);
+    }
+
+    @KafkaListener(topics = {TOPIC_NAME + ".message", TOPIC_NAME + ".booking"})
+    public void messageAndBookTopics(@Payload String message,
+                                     @Header(KafkaHeaders.RECEIVED_TOPIC) String receivedTopic,
+                                     @Header(KafkaHeaders.RECEIVED_PARTITION) String receivedPartition,
+                                     @Header(KafkaHeaders.OFFSET) String receivedOffset) {
+        log.info("Recive from Topic {} by partition {}, offset {}: {}", receivedTopic, receivedPartition, receivedOffset, message);
     }
 }
